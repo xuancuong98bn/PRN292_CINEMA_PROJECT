7179186c628a53daf87b9a5d38b450d6569cf301
xuancuong98bn <xuancuong98bn@gmail.com>
2019-07-23 22:26:29 +0700

    Update detail film

---
+++ cinema/cinema/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cinema.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ cinema/cinema/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using cinema.Contexts;
+using cinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cinema.Controllers
+{
+    public class MoviesController : Controller
+    {
+        DBContext db = new DBContext();
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(db.Films.ToList());
+        }
+
+        // GET: Movies/Detail/5
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film f = db.Films.Find(id);
+            if (f == null)
+            {
+                return HttpNotFound();
+            }
+            return View(f);
+        }
+    }
+}
