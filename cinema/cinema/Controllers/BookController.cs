b90357c5645bbcc584c7aafa8508adc86c3bfc6e
Cuong Nguyen Xuan <xuancuong98bn@gmail.com>
2019-07-24 11:35:44 +0700

    Step2 of booking completed

--- cinema/cinema/Controllers/BookController.cs
+++ cinema/cinema/Controllers/BookController.cs
@@ -48,9 +48,56 @@ namespace cinema.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Step2(BookingView bkv, int ShowtimeID)
+        public ActionResult Step2(int FilmID, int ShowtimeID, int RoomID)
         {
-            return View();
+            if (ShowtimeID != 0 && RoomID != 0)
+            {
+                var CurrFilm = db.Films.Find(FilmID); 
+                Session.Add("ShowtimeID", ShowtimeID);
+                Session.Add("RoomID", RoomID);
+                Session.Add("CurrFilm", CurrFilm);
+                var seats = db.Seats.Where(s => s.RoomID == RoomID).ToList();
+                ViewBag.SeatTypes = db.Seattypes.ToList();
+                ViewBag.Room = db.Rooms.Find(RoomID);
+                ViewBag.CurrFilm = CurrFilm;
+                return View(seats);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Step3(int row, int column)
+        {
+            if (row != 0 && column != 0)
+            {
+                var roomID = (int) Session["RoomID"];
+                var seat = db.Seats.Where(s => s.RoomID == roomID && s.Rowth == row && s.Columnth == column).SingleOrDefault();
+                if (seat == null || seat.IsEnable == false)
+                {
+                    RedirectToAction("Index", "Movies");
+                }
+                else
+                {
+
+                }
+                var CurrFilm = db.Films.Find(FilmID);
+                Session.Add("ShowtimeID", ShowtimeID);
+                Session.Add("RoomID", RoomID);
+                Session.Add("CurrFilm", CurrFilm);
+                var seats = db.Seats.Where(s => s.RoomID == RoomID).ToList();
+                ViewBag.SeatTypes = db.Seattypes.ToList();
+                ViewBag.Room = db.Rooms.Find(RoomID);
+                ViewBag.CurrFilm = CurrFilm;
+                return View(seats);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
