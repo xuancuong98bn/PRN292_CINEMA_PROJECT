31ddbac3bebf18aeb70ecf12b6138e2890b6e965
Cuong Nguyen Xuan <xuancuong98bn@gmail.com>
2019-08-06 13:19:59 +0700

    Auto stash before merge of "master" and "origin/khanhntn"

---
+++ cinema/cinema/Areas/Admin/Controllers/SeatsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using cinema.Contexts;
+using cinema.Models;
+
+namespace cinema.Areas.Admin.Controllers
+{
+    public class SeatsController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Admin/Seats
+        public ActionResult Index(String keyword, int seattypeCode = 0)
+        {
+            var seattypes = db.Seattypes.ToList();
+            ViewBag.SeattypeID = new SelectList(seattypes, "Code", "Name");
+            return View(db.Seats.ToList());
+        }
+
+        // GET: Admin/Seats/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seat seat = db.Seats.Find(id);
+            if (seat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seat);
+        }
+
+        // GET: Admin/Seats/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        // GET: Admin/Seats/Create
+        public ActionResult Create(int RoomId)
+        {
+            Seat seat = new Seat();
+            seat.RoomID = RoomId;
+            return View(seat);
+        }
+
+        // POST: Admin/Seats/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,RoomID,Rowth,Columnth,IsEnable")] Seat seat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Seats.Add(seat);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(seat);
+        }
+
+        // GET: Admin/Seats/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seat seat = db.Seats.Find(id);
+            if (seat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seat);
+        }
+
+        // POST: Admin/Seats/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,RoomID,Rowth,Columnth,IsEnable")] Seat seat)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seat).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seat);
+        }
+
+        // GET: Admin/Seats/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seat seat = db.Seats.Find(id);
+            if (seat == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seat);
+        }
+
+        // POST: Admin/Seats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Seat seat = db.Seats.Find(id);
+            db.Seats.Remove(seat);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
